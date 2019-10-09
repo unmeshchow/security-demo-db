d9a9fbf21d985fe9215772f1bf63e919ac87a739
Unmesh Chowdhury <unmeshchow@gmail.com>
2019-10-09 20:28:42 +0600

    Initial commit

---
+++ src/main/java/com/unmeshc/security/SecurityDemoDbApplication.java
@@ -0,0 +1,13 @@
+package com.unmeshc.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDemoDbApplication.class, args);
+    }
+
+}
